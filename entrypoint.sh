a7119812297348dbccce53258e77c6416f3f8888
Azzam Amal Islami <azzam@diqit.io>
2020-10-08 10:58:50 +0700

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
   
-# turn on bash's job control
-set -m
-
-./sbin/runit-wrapper &
-  
-# Start the primary process and put it in the background
 chown -R www-data:www-data /var/www/html
-  
-# the my_helper_process might need to know how to wait on the
-# primary process to start before it does its work and returns
-  
-  
-# now we bring the primary process back into the foreground
-# and leave it there
-fg %1
+./sbin/runit-wrapper
